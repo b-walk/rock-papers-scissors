8ee05706216d08a4122047b975fa56fa9f767883
Blake <blameswalker@gmail.com>
2023-03-13 02:33:18 -0700

    Make function that plays one round of RPS then logs the result to the console

--- script.js
+++ script.js
@@ -10,3 +10,33 @@ function getComputerChoice() {
 const drawMessage = 'Draw!';
 const winMessage = 'Player wins!';
 const loseMessage = 'Computer wins!';
+
+function playRound(playerChoice, computerChoice) {
+    let choiceCombination = playerChoice + computerChoice;
+    if (playerChoice === computerChoice) {
+        console.log(drawMessage);
+    };
+    switch (choiceCombination) {
+        case 'rockpaper':
+            console.log(loseMessage);
+            break;
+        case 'rockscissors':
+            console.log(winMessage);
+            break;
+        case 'paperrock':
+            console.log(winMessage);
+            break;
+        case 'paperscissors':
+            console.log(loseMessage);
+            break;
+        case 'scissorsrock':
+            console.log(loseMessage);
+            break;
+        case 'scissorspaper':
+            console.log(winMessage);
+            break;
+        default:
+            console.log('SWITCH_STATEMENT_ERROR');
+    }
+
+}
