10ea5e37a2e381a34a898610ba2d16e7d90cbfd6
Blake <blameswalker@gmail.com>
2023-03-13 02:16:46 -0700

    Create function that randomly generates a computer choice of rock, paper, or scissors

---
+++ script.js
@@ -0,0 +1,9 @@
+let computerChoice;
+getComputerChoice();
+function getComputerChoice() {
+    const randomNum = Math.floor(Math.random() * 3);
+    (randomNum === 0) ? computerChoice = 'rock' :
+    (randomNum === 1) ? computerChoice = 'paper' :
+    computerChoice = 'scissors';
+    console.log(randomNum, computerChoice);
+}
