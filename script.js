a999146549c98fd118317a36aa6f4e24099bdea6
Blake <blameswalker@gmail.com>
2023-03-19 22:16:13 -0700

    Add title, scoreboard, notification box, and buttons for the player in HTML

--- script.js
+++ script.js
@@ -10,36 +10,3 @@ let computerChoice;
 const drawMessage = 'Draw!';
 const winMessage = 'Victory!';
 const loseMessage = 'Defeat!';
-
-function playRound(playerChoice, computerChoice) {
-    let choiceCombination = playerChoice + computerChoice;
-    if (playerChoice === computerChoice) {
-        console.log(drawMessage);
-    };
-    switch (choiceCombination) {
-        case 'rockpaper':
-            console.log(`${loseMessage} Paper beats rock!`);
-            computerScore += 1;
-            break;
-        case 'rockscissors':
-            console.log(`${winMessage} Rock beats scissors!`);
-            playerScore += 1;
-            break;
-        case 'paperrock':
-            console.log(`${winMessage} Paper beats rock!`);
-            playerScore += 1;
-            break;
-        case 'paperscissors':
-            console.log(`${loseMessage} Scissors beat paper!`);
-            computerScore += 1;
-            break;
-        case 'scissorsrock':
-            console.log(`${loseMessage} Rock beats scissors!`);
-            computerScore += 1;
-            break;
-        case 'scissorspaper':
-            console.log(`${winMessage} Scissors beat paper!`);
-            playerScore += 1;
-    }
-
-}
